55d76c2cb04e4ff74b580e4338674cd485a5cd07
adityakumarbind96-prog <adityakumarbind96@gmail.com>
2026-03-09 13:30:02 +0530

    Create Message component for displaying a secret message

---
+++ app/app/message/page.js
@@ -0,0 +1,26 @@
+export default function Message() {
+  return (
+    <div style={{
+      height:"100vh",
+      display:"flex",
+      justifyContent:"center",
+      alignItems:"center",
+      textAlign:"center",
+      background:"pink",
+      fontFamily:"sans-serif"
+    }}>
+
+      <div>
+        <h1>💖 Secret Message 💖</h1>
+
+        <p style={{fontSize:"20px",maxWidth:"400px"}}>
+        You are the most special friend in my life.
+        Thank you for always supporting me.
+        I am lucky to have a friend like you. 💖
+        </p>
+
+      </div>
+
+    </div>
+  );
+}
